29a2aa8e4b5f3a51a638b934a19cb4f242e12aec
aspcodenet <stefan.holmberg@systementor.se>
2024-01-04 08:57:13 +0100

    Add project files.

---
+++ NattBatch/App.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NattBatch
+{
+    internal class App
+    {
+        internal void Run()
+        {
+            var nameService = new NameService();
+            while (true)
+            {
+                Console.WriteLine("Ange id:");
+                int id = Convert.ToInt32(Console.ReadLine());
+                var name = nameService.GetName(id);
+                Console.WriteLine(name);
+
+            }
+        }
+    }
+}
---
+++ NattBatch/NameService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NattBatch
+{
+    internal class NameService
+    {
+        public string GetName(int id)
+        {
+            return $"Hello-{id}";
+        }
+    }
+}
