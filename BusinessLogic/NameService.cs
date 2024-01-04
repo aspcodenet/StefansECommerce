31d5362eaf0278687448ca5708c40e53f6904fe5
aspcodenet <stefan.holmberg@systementor.se>
2024-01-04 09:47:42 +0100

    a

---
+++ BusinessLogic/NameService.cs
@@ -0,0 +1,22 @@
+﻿using BusinessLogic.Model;
+
+namespace BusinessLogic
+{
+    public class NameService
+    {
+        NorthWindContext context;
+        public NameService()
+        {
+            context = new NorthWindContext();
+        }
+        public string GetName(int id)
+        {
+
+            var p = context.Products.FirstOrDefault(p=>p.ProductId == id);
+            if (p == null) return "";
+            return p.ProductName;
+            
+        }
+
+    }
+}
--- NattBatch/App.cs
+++ NattBatch/App.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BusinessLogic;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,6 +7,19 @@ using System.Threading.Tasks;
 
 namespace NattBatch
 {
+    //  1. I morgon ska vi skapa En WebApp som ska använda vår coola 
+    //      nameservice.GetName(id);
+
+    // 2. Så vi skapar en ClassLibrary och lägger koden där!
+
+    // 3. Vi ska också göra så att NameService.GetName() läser från Databas - northwind
+    // dvs returnerar produktnamn med det id:t
+    // dvs vi behöver ett DBContext
+
+    // 4. Vi scaffoldar Databas
+
+
+
     internal class App
     {
         internal void Run()
